22d776682b15b53b120403340b34a08b43006ed7
aceneil <123354319@qq.com>
2024-02-26 23:57:08 +0800

    Site updated: 2024-02-26 23:57:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/06/hello-world/"];function toRandomPost(){
+var posts=["posts/2583/","posts/b295/","posts/670b/","posts/9fc1e5d1/","posts/22b6/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
